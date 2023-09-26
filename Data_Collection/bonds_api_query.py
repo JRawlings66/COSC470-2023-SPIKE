96c4930b5989f430e532caabce3018f3a3f56bc1
DylanSoares <Dylan.Soares@myokanagan.bc.ca>
2023-09-26 09:12:11 -0700

    Added bonds_api_query.py

---
+++ Data_Collection/bonds_api_query.py
@@ -0,0 +1,11 @@
+# Get data from the bonds API list file
+# Parse the json so we know what our URL and keys are
+# Iterate through the stocks using the key and url, then move onto the next API
+
+def load_config():
+    return []
+
+
+# code to only be executed if ran as script
+if __name__ == "__main__":
+    JSON_config = load_config()
