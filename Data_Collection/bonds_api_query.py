43d9a3dbb639c72005426f44779a985d398b8ea5
DylanSoares <Dylan.Soares@myokanagan.bc.ca>
2023-09-26 11:40:37 -0700

    Added bonds_api_query.py

--- Data_Collection/bonds_api_query.py
+++ Data_Collection/bonds_api_query.py
@@ -1,11 +1,18 @@
 # Get data from the bonds API list file
 # Parse the json so we know what our URL and keys are
 # Iterate through the stocks using the key and url, then move onto the next API
+import json
+
 
 def load_config():
-    return []
+    config_file = open("Bonds_List.json", "r")
+    config = json.load(config_file)
+    return config
 
+# def make_queries():
+#     for api in len()
 
 # code to only be executed if ran as script
 if __name__ == "__main__":
     JSON_config = load_config()
+    print(JSON_config[0]['date_windows'])
