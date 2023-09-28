5fb1dbe57a4932b4197021ccb2c477ba5e6301c3
dakota <40772063+irradiated@users.noreply.github.com>
2023-09-27 22:41:43 -0700

    Update db_test.py

--- Unit_Tests/db_test.py
+++ Unit_Tests/db_test.py
@@ -32,7 +32,7 @@ try:
     engine = create_engine(uri)
 
     # test insert statement
-    sql = sql.text(f"INSERT INTO {'2yr_bonds'} ({'Date'}, {'Rate'}) VALUES (curdate(), 1.0)")
+    sql = sqlalchemy.text(f"INSERT INTO {'2yr_bonds'} ({'Date'}, {'Rate'}) VALUES (curdate(), 1.0)")
 
     with engine.connect() as conn:
         result = conn.execute(sql)
