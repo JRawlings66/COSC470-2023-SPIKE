879f96eaca5fbe2f6675bde5d324589c7b242b4b
Jake Fischer <jakefischer19@gmail.com>
2023-09-28 13:35:07 -0700

    (CS-100)boiler plate unit test for bonds api query

---
+++ Data_Collection/test_bonds_api_query.py
@@ -0,0 +1,16 @@
+import unittest
+import bonds_api_query
+
+class TestBondsAPIQuery(unittest.TestCase):
+
+    def test_load_config(self):
+        #
+
+    def test_make_queries(self):
+        
+    
+    def test_write_file(self):
+
+
+if __name__ == '__main__':
+    unittest.main()
