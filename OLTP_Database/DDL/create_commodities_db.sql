286853c02f3d9455f5529e63d023a3e2eb964c5d
Jake Fischer <jakefischer19@gmail.com>
2023-10-02 19:10:04 -0700

    (CS-131) updated DDL for new database schema

--- OLTP_Database/DDL/create_commodities_db.sql
+++ OLTP_Database/DDL/create_commodities_db.sql
@@ -2,20 +2,21 @@ CREATE DATABASE IF NOT EXISTS commodity_db;
 
 USE commodity_db;
 
-CREATE TABLE `commodity_list` (
-  `ID ` INTEGER,
+CREATE TABLE `Commodity_List` (
+  `ID` BIGINT,
   `Name` VARCHAR(30),
   `Symbol` VARCHAR(5),
-  PRIMARY KEY (`ID `)
+  PRIMARY KEY (`ID`)
 );
 
-CREATE TABLE `commodity_values` (
-  `CommodityID ` INTEGER,
+CREATE TABLE `Commodity_Values` (
+  `CommodityID` BIGINT,
   `Date` DATETIME,
   `Open` DECIMAL,
   `High` DECIMAL,
   `Low` DECIMAL,
   `Close` DECIMAL,
   `Volume` DECIMAL,
-  PRIMARY KEY (`CommodityID `, `Date`)
+  PRIMARY KEY (`CommodityID`, `Date`),
+  FOREIGN KEY (`CommodityID`) REFERENCES `Commodity_List`(`ID`)
 );
