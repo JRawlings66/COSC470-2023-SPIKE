a75b86e0b37ce8a98113ae8e111ea2988e161b30
Jacob Labelle <jacob.labelle93@gmail.com>
2023-10-02 19:04:49 -0700

    CS-156 Insertion Triggers
    Created triggers for
    - Commodities
    - Companies
    - Indexes

---
+++ OLTP_Database/Triggers/indexes_trg.sql
@@ -0,0 +1,28 @@
+DELIMITER $$
+CREATE TRIGGER indices_id_gen_trg
+BEFORE INSERT ON Indices
+FOR EACH ROW
+BEGIN
+	DECLARE new_id BIGINT;
+	DECLARE temp INTEGER;
+	DECLARE valid INTEGER;
+    SET new_id = UUID_SHORT();
+    SET valid = 1;
+	SELECT COUNT(*) INTO temp FROM Indices WHERE Symbol = NEW.Symbol;
+	IF (temp = 0) THEN
+    	WHILE (valid = 1) DO
+        	SELECT COUNT(*) INTO temp FROM Indices WHERE ID = new_id;
+        	IF (temp = 1) THEN
+            	SET new_id = UUID_SHORT();
+   		 ELSE
+   		 	SET valid = 0;
+        	END IF;
+    	END WHILE;
+    ELSE
+    	SIGNAL SQLSTATE '45000'
+   	 SET MESSAGE_TEXT = 'A record for this index already exists.';
+    END IF;
+	SET NEW.ID = new_id;
+END;
+$$
+DELIMITER ;
