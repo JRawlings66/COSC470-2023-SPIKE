87f40419e04087109f231b5d3b9a8f707a7128a3
AwesomeAim <30537683+AwesomeAim@users.noreply.github.com>
2023-10-05 03:40:03 -0700

    full null test

--- Unit_Tests/null_test_premium.py
+++ Unit_Tests/null_test_premium.py
@@ -25,7 +25,7 @@ from sqlalchemy.exc import (
 db = "bonds"
 sql_port = 3306
 # load database credentials dict
-creds = credentials.databases[db]
+creds = None
 uri = None
 # connect to mySQL server
 engine = None
@@ -33,6 +33,7 @@ engine = None
 class NullTest(unittest.TestCase):
     
     def test_bonds(self):
+        creds = credentials.databases['bonds']
         uri = f"mysql+pymysql://{creds['user']}:{creds['pass']}@{creds['host']}:{sql_port}/{creds['database']}"
         engine = create_engine(uri)
         with self.assertRaises(IntegrityError):
@@ -40,14 +41,72 @@ class NullTest(unittest.TestCase):
                 conn.execute(text("insert into `Bonds` values (null, null, null)"))
                 conn.commit()
                 
-    def test_bonds2(self):
+    def test_index_values(self):
+        creds = credentials.databases['index']
         uri = f"mysql+pymysql://{creds['user']}:{creds['pass']}@{creds['host']}:{sql_port}/{creds['database']}"
         engine = create_engine(uri)
         with self.assertRaises(IntegrityError):
             with engine.connect() as conn:
-                conn.execute(text("insert into `Bonds` values (null, null, null)"))
-                conn.commit()        
-
+                conn.execute(text("insert into `Index_Values` values (null, null, null, null, null, null, null)"))
+                conn.commit()
+                
+    def test_indices(self):
+        uri = f"mysql+pymysql://{creds['user']}:{creds['pass']}@{creds['host']}:{sql_port}/{creds['database']}"
+        engine = create_engine(uri)
+        with self.assertRaises(IntegrityError):
+            with engine.connect() as conn:
+                conn.execute(text("insert into `Indices` values (null, null, null)"))
+                conn.commit()
+                
+    def test_index_values(self):
+        creds = credentials.databases['companies']
+        uri = f"mysql+pymysql://{creds['user']}:{creds['pass']}@{creds['host']}:{sql_port}/{creds['database']}"
+        engine = create_engine(uri)
+        with self.assertRaises(IntegrityError):
+            with engine.connect() as conn:
+                conn.execute(text("insert into `Changelogs` values (null, null, null, null, null, null, null, null)"))
+                conn.commit()
+                
+    def test_companies(self):
+        uri = f"mysql+pymysql://{creds['user']}:{creds['pass']}@{creds['host']}:{sql_port}/{creds['database']}"
+        engine = create_engine(uri)
+        with self.assertRaises(IntegrityError):
+            with engine.connect() as conn:
+                conn.execute(text("insert into `Companies` values (null, null, null)"))
+                conn.commit()            
+                
+    def test_stock_values(self):
+        uri = f"mysql+pymysql://{creds['user']}:{creds['pass']}@{creds['host']}:{sql_port}/{creds['database']}"
+        engine = create_engine(uri)
+        with self.assertRaises(IntegrityError):
+            with engine.connect() as conn:
+                conn.execute(text("insert into `Stock_Values` values (null, null, null, null, null, null, null, null)"))
+                conn.commit()                        
+                
+    def test_company_statements(self):
+        uri = f"mysql+pymysql://{creds['user']}:{creds['pass']}@{creds['host']}:{sql_port}/{creds['database']}"
+        engine = create_engine(uri)
+        with self.assertRaises(IntegrityError):
+            with engine.connect() as conn:
+                conn.execute(text("insert into `Company_Statements` values (null, null, null, null, null, null, null, null, null, null, null, null, null, null)"))
+                conn.commit()             
+                
+    def test_commodity_list(self):
+        creds = credentials.databases['commodities']
+        uri = f"mysql+pymysql://{creds['user']}:{creds['pass']}@{creds['host']}:{sql_port}/{creds['database']}"
+        engine = create_engine(uri)
+        with self.assertRaises(IntegrityError):
+            with engine.connect() as conn:
+                conn.execute(text("insert into `Commodity_List` values (null, null, null)"))
+                conn.commit()            
+                
+    def test_commodity_values(self):
+        uri = f"mysql+pymysql://{creds['user']}:{creds['pass']}@{creds['host']}:{sql_port}/{creds['database']}"
+        engine = create_engine(uri)
+        with self.assertRaises(IntegrityError):
+            with engine.connect() as conn:
+                conn.execute(text("insert into `Commodity_Values` values (null, null, null, null, null, null, null)"))
+                conn.commit()            
 
     #print(f"Test failed, null values inserted.")
     #print(f"Null values not inserted. Test successful.")
